86e642a028f66e9b568290262575c5b6dc6010c2
soumboul fall <likadiop091955@gmail.com>
2025-07-04 22:45:39 +0100

    tontine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Clients from "./pages/Clients";
@@ -9,89 +8,30 @@ import Settings from "./pages/Settings";
 import Login from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Toast from "./components/Toast";
-import Sidebar from "./components/Sidebar";
-import BottomNav from "./components/BottomNav";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [toast, setToast] = useState({ message: "", type: "success" });
-
   const showToast = (message, type = "success") => {
     setToast({ message, type });
     setTimeout(() => setToast({ message: "", type }), 2500);
   };
-
   return (
-    <Router>
-      <div className="flex flex-col md:flex-row min-h-screen bg-light-gray">
-        {/* Sidebar (hidden on mobile) */}
-        <Sidebar />
-
-        <main className="flex-1 p-4 md:p-8">
-          <Routes>
-            <Route path="/login" element={<Login showToast={showToast} />} />
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Dashboard showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/clients"
-              element={
-                <PrivateRoute>
-                  <Clients showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/members"
-              element={
-                <PrivateRoute>
-                  <Members showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/debts"
-              element={
-                <PrivateRoute>
-                  <Debts showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/reports"
-              element={
-                <PrivateRoute>
-                  <Reports showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/settings"
-              element={
-                <PrivateRoute>
-                  <Settings showToast={showToast} />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </main>
-
-        {/* Bottom navigation (mobile only) */}
-        <BottomNav />
-
-        {/* Toast */}
-        <Toast
-          message={toast.message}
-          type={toast.type}
-          onClose={() => setToast({ message: "", type: toast.type })}
-        />
-      </div>
-    </Router>
+    <div className="w-full min-h-screen bg-light-gray">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login showToast={showToast} />} />
+          <Route path="/" element={<PrivateRoute><Dashboard showToast={showToast} /></PrivateRoute>} />
+          <Route path="/clients" element={<PrivateRoute><Clients showToast={showToast} /></PrivateRoute>} />
+          <Route path="/members" element={<PrivateRoute><Members showToast={showToast} /></PrivateRoute>} />
+          <Route path="/debts" element={<PrivateRoute><Debts showToast={showToast} /></PrivateRoute>} />
+          <Route path="/reports" element={<PrivateRoute><Reports showToast={showToast} /></PrivateRoute>} />
+          <Route path="/settings" element={<PrivateRoute><Settings showToast={showToast} /></PrivateRoute>} />
+        </Routes>
+        <Toast message={toast.message} type={toast.type} onClose={() => setToast({ message: "", type: toast.type })} />
+      </Router>
+    </div>
   );
 }
 
